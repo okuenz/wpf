d76556c2ce1a474bd7310efcd89acb76bacb2851
Masha <ivzmaria9@gmail.com>
2023-12-05 16:06:09 +0300

    first commit

---
+++ WPF Tutorial/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WPF_Tutorial
+{
+    public partial class MainWindow : Window
+    {
+        /*bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+
+            }
+            else 
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            
+            running = !running;*/
+        //}
+    }
+}
